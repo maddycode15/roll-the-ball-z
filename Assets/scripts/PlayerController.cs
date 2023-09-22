4fafcabbfd599492e412af08514e19254f809789
matthew allison restin <144450180+maddycode15@users.noreply.github.com>
2023-09-22 10:34:31 -0300

    count text now counts
    game runs, the count text is now displayed on the ui

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;               // this is the part of the code that tells visual studio to comunicate with the unity engine
 using UnityEngine.InputSystem;   //this is the part of the code that lets unity comunicate with the input system
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10; // this public float creates a float and makes it public. being public allows the value to be controlled by the unity editor. this one is going to control spe
-    
+    public TextMeshProUGUI CountText; // this line creates a special variable, a TextMeshProUGUI variable is spesifically designed to work with text, the variables name is count text
+
    private Rigidbody rb;  // private means that this script can not be accessed from the inspector or from other scripts. public would allow it to be accessed this way
     private int count; // this creates a private integer variable in this script. the variable will be used to hold a number and that number will be used to count how many PickUP cubes have been collected.
     private float movementX;
@@ -18,6 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         rb =GetComponent<Rigidbody>();  // this gives the rb variable information attached to the rigidbody component attached to this game object. (makes the variable = to the physics thing attached to the game object)
         count = 0;  // this line sets the starting value of the count variable. it starts at zero because there are zero cubes collected at the start
+        SetCountText(); // set count text is here because it makes the count text display, it starts at zero so this will only display Count:
     }
     void OnMove(InputValue movementValue)  //the void part of this function (on move in this case) means that it wont return any values. (movement value captures the movement of the ball on the x and y axis) this data is stored in a vector2 variable
     {
@@ -25,6 +28,10 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;   // i am pretty sure that these lines cause the movementx and y float respectivly to be = to their respective movment values
         movementY = movementVector.y;
     }
+
+    void SetCountText() // this is a method, you are using a method to manage the count text function
+    {
+        CountText.text = "Count: " + count.ToString();    }  // CountText.text is the actual text displayed in game, this line of code says it should display, count: plus the string value of the count variable
 
     void FixedUpdate()
     {
@@ -39,6 +46,8 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);  // this is the code that runs when the collider trigger is collected. it deactivates the game object by selecting set active, a property that determines if the game object is active to false making the fact that it is active untrue
             count = count + 1; // this line changes the value of the count variabke to the current value of count plus 1, essentially this just adds 1 to the current count each time a cube is picked up
+
+            SetCountText(); // this makes the count text go up each time the count goes up
         }
          
     }
